dce31ab0b071a13022dc3a193a824a669446aa53
Yash <yashasdr327@gmail.com>
2024-10-31 13:55:09 +0530

    first update

---
+++ heading.py
@@ -0,0 +1,32 @@
+def left_heading(heading):
+
+    if heading.lower() == 'n':
+        new_heading = 'w'
+
+    elif heading.lower() == 'w':
+        new_heading = 's'
+
+    elif heading.lower() == 's':
+        new_heading = 'e'
+
+    else:
+        new_heading = 'n'
+
+    print(new_heading)
+    return new_heading
+
+def right_heading(heading):
+    if heading.lower() == 'n':
+        new_heading = 'e'
+
+    elif heading.lower() == 'e':
+        new_heading = 's'
+
+    elif heading.lower() == 's':
+        new_heading = 'w'
+
+    else:
+        new_heading = 'n'
+
+    print(new_heading)
+    return new_heading
