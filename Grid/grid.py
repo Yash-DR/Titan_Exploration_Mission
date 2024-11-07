389f0973f13f7d59bcd447b873be8bd69f235a33
Yash <yashasdr327@gmail.com>
2024-11-07 12:07:04 +0530

    To check if the initial position exists within the grid

---
+++ Grid/grid.py
@@ -0,0 +1,18 @@
+class GridBoundary:
+    def __init__(self, total_grid_size):
+        self.total_grid_size = total_grid_size
+        self.x_max = self.total_grid_size[0]
+        self.y_max = self.total_grid_size[1]
+        self.x_min = 0
+        self.y_min = 0
+
+
+    def coordinate_position(self, initial_coordinate):
+        print(initial_coordinate)
+        if self.x_min <= int(initial_coordinate[0]) <= self.x_max and self.y_min <= int(initial_coordinate[1]) <= self.y_max:
+            return True
+        else:
+            return False
+
+
+
--- main.py
+++ main.py
@@ -6,52 +6,25 @@ The first line of input is upper right co-ordinate of plateau., the lower left c
 The first line gives the robot position, and the second line is a series of instructions telling the robot how to explore the plateau.
 The position is made up of 2 letters and a letter separated by spaces"""
 
-from heading import left_heading, right_heading
-from movement import new_coordinates
 
-num_of_robots = input("Enter the number of Robots - ")
-xy = input("Upper right co-ordinates - ")
-x_ord = []
-y_ord = []
-heading = []
-exploration_values = []
-x_ord_max = None
-y_ord_max = None
+from Grid.grid import GridBoundary
 
-if len(xy) == 2:
-    lower_left_ord = [0, 0]
-    upper_right_ord = [xy[0], xy[1]]
-    x_ord_max = xy[0]
-    y_ord_max = xy[1]
 
-    for i in range(int(num_of_robots)):
-        co_ordinates = input(f"Enter the co-ordinates of the Robot-{i+1} - ")
-        if len(co_ordinates) == 3:
-            x_ord.append(co_ordinates[0])
-            y_ord.append(co_ordinates[1])
-            heading.append(co_ordinates[2])
+def main():
+    # Defining Input Values
+    total_grid_size = (5, 5)
+    robot_initial_coordinate = [('1', '2', 'N'), ('3', '3', 'E')]
+    robot_commands = ["LMLMLMLMM", "MMRMMRMRRM"]
 
-            navigation = input("Enter the instructions to explore the plateau - ")
-            exploration_values.append(navigation)
+    # Check if the robot initial_coordinate position is within the grid
+    for coordinate in robot_initial_coordinate:
+        position = GridBoundary(total_grid_size).coordinate_position(coordinate)
+        if position:
+            print(position)
 
-        else:
-            print("Invalid Robot co-ordinates")
 
-else:
-    print("invalid co-ordinates")
 
-for j in range(int(num_of_robots)):
-    try:
-            for i in exploration_values[j]:
-                if i.lower() == 'l':
-                    heading[j] = left_heading(heading[j])
-                elif i.lower() == 'r':
-                    heading[j] = right_heading(heading[j])
-                elif i.lower() == 'm':
-                    x_ord[j], y_ord[j] = new_coordinates(x_ord_max, y_ord_max, x_ord[j], y_ord[j], heading[j])
 
-            print(f"New co-ordinates for Robot-{j+1} is {x_ord[j]}{y_ord[j]}{heading[j].upper()}")
-
-    except:
-        print(f"Cannot find the co-ordinates for Robot-{j+1} due to incorrect inputs")
+if __name__ == "__main__":
+    main()
 
